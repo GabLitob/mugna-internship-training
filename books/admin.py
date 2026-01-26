a06cb5993d5f4b2124d3b0ede591c2d2cffb4b5b
GabLitob <litobangelgabrieljr@gmail.com>
2026-01-26 14:19:33 +0800

    creating a book admin

--- books/admin.py
+++ books/admin.py
@@ -3,6 +3,17 @@ from django.contrib import admin
 from .models import Author, Book, Publisher
 
 
-admin.site.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    fields = ["email", "first_name", "last_name"]
+
+class BookAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {"fields": ["title", "authors", "publisher"]}),
+        ("Date information", {
+            "fields": ["publication_date"],
+        }),
+    ]
+
+admin.site.register(Author, AuthorAdmin)
 admin.site.register(Book)
 admin.site.register(Publisher)
